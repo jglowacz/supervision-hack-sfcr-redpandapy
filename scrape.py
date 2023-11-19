a3e63bac12ba078364ca9ad2472b7eb3e87e7589
jglowacz <46658025+jglowacz@users.noreply.github.com>
2023-11-19 08:09:29 +0100

    scrape nearly final version

--- scrape.py
+++ scrape.py
@@ -1,92 +1,129 @@
-# import requests
-# from bs4 import BeautifulSoup
-# import os
+# Step 1 - Get the list of URLs
+# Step 2 - Loop through the list of URLs
+# Step 3 - Get the HTML for each URL
+# Step 4 - Parse the HTML to get the links to the PDFs
+# Step 5 - Download the PDFs
+# Step 6 - Save the PDFs to disk
+# Step 7 - Repeat for each URL
 
-#  Load the data from the JSON file
-# with open('sfcr_sites.json', 'r') as f:
-#     merged_data = json.load(f)
-
-merged_data = [
-    # "https://www.allianz.pl/pl_PL/dla-ciebie/o-allianz/tu-allianz-zycie.html#sfcr",
-    "https://www.allianz.pl/pl_PL/dla-ciebie/o-allianz/tuir.html#sfcr",
-    # "https://cardif.pl/onas/informacje-finansowe-i-podatkowe",
-    # "https://ca-ubezpieczenia.pl/ca-towarzystwo-ubezpieczen",
-    # "https://www.compensa.pl/o-nas/#raporty",
-    # "https://www.ergohestia.pl/o-ergo-hestii/wyniki-finansowe-i-raporty/",
-    # "https://www.generali.pl/dla-ciebie/informacje-korporacyjne",
-    # "https://interpolska.pl/grupa-inter/sprawozdania/",
-    # "https://www.nn.pl/notowania-i-wyniki-finansowe/raporty-finansowe",
-    # "https://www.openlife.pl/o-nas/raporty-publiczne/",
-    # "https://pkoubezpieczenia.pl/pko-zycie-towarzystwo-ubezpieczen",
-    # "https://www.pocztowenazycie.pl/strefa-klienta-dokumenty",
-    # "https://polskigaztuw.pl/a_pgtuw_sfcr/",
-    # "https://www.pzu.pl/relacje-inwestorskie/raporty",
-    # "https://rejentlife.com.pl/ujawnienia-publiczne/",
-    # "https://saltus.pl/o-nas/saltus_tu_zycie_sa/",
-    # "https://www.santander.allianz.pl/raporty-roczne/",
-    # "https://www.signal-iduna.pl/o-nas/sprawozdania-finansowe/",
-    # "https://tueuropa.pl/raporty-roczne",
-    # "https://www.uniqa.pl/centrum-informacyjne/wyniki-finansowe/"
-]
-
-# # Loop through the list of URLs
-# for data in merged_data:
-#     url = data
-#     try:
-#         response = requests.get(url)
-#         soup = BeautifulSoup(response.text, 'html.parser')
-
-#         # Find all links to PDF files
-#         pdf_links = [(a['href'], a.text) for a in soup.find_all('a', href=True) if a['href'].endswith('.pdf')]
-
-#         # Write the links to a file
-#         with open("scrapepdfs.txt", "a") as f:
-#             for link, text in pdf_links:
-#                 if "https" not in link:
-#                     link = url + link
-#                 f.write(f"{link}\t{text}\n")
-#     except requests.exceptions.RequestException as e:
-#         print(f"An error occurred with URL {url}: {e}")
-
-        
-    # Download and save each PDF
-    # for pdf_url in pdf_links:
-    #     if 'http' not in pdf_url:
-    #         pdf_url = url + pdf_url
-    #     pdf_response = requests.get(pdf_url)
-    #     with open(os.path.join("pdfs", pdf_url.split("/")[-1]), 'wb') as f:
-    #         f.write(pdf_response.content)
-    
+# TODO: Implement async version of the script
+# import aiohttp
+# import asyncio
+import json
 import requests
 from bs4 import BeautifulSoup
 import os
 from urllib.parse import urljoin
+from unidecode import unidecode
+
+SFCR_SITES_PATH = "sfcr_sites.json"
+
+# Load the data from the JSON file
+with open(SFCR_SITES_PATH, "r") as f:
+    sfcr_sites = json.load(f)
+
+# Extract the 'url_final_page' from each object in the JSON array
+merged_data = [site["url_final_page"] for site in sfcr_sites]
+
+opinions_keywords = [
+    "biegł",
+    "rewident",
+    "opini",
+    "badani",
+    "raport",
+    "niezalezn",
+    "audytor"
+]
+
+sfcr_keywords = [
+    "sprawozdanie o"
+    "wypłacalności",
+    "kondycji finansowej",
+    "Solvency and financial condition report",
+    "SFCR"
+]
+
+# 2 years as a testing period, can be changed to any value
+years_keywords = ["2021", "2022"]
+
+opinions_keywords_uni = [unidecode(x.lower()) for x in opinions_keywords]
+sfcr_keywords_uni = [unidecode(x.lower()) for x in sfcr_keywords]
+
+searched = [opinions_keywords, opinions_keywords_uni, sfcr_keywords, sfcr_keywords_uni]
+
 
 def get_soup(url):
     response = requests.get(url)
-    return BeautifulSoup(response.text, 'html.parser')
+    return BeautifulSoup(response.text, "html.parser")
 
-def get_pdf_links(soup, base_url):
-    links = [(a['href'], a.text) for a in soup.find_all('a', href=True) 
-             if a['href'].endswith('.pdf') and 
-             ('sfcr' in a.text.lower() or 'sprawozdanie' in a.text.lower() or 'wyp' in a.text.lower())]
-    absolute_links = [(urljoin(base_url, link), text) for link, text in links]
-    return absolute_links
 
-def write_links_to_file(links, filename):
-    with open(filename, "a") as f:
-        for link, text in links:
-            f.write(f"{link}\t{text}\n")
+def get_pdf_links(soup, base_url, searched, years_keywords):
+    links = []
+    for a in soup.find_all('a', href=True):
+        if a['href'].endswith('.pdf'):
+            for keywords in searched:
+                for keyword in keywords:
+                    if keyword in a.text.lower():
+                        for year in years_keywords:
+                            if year in a.text.lower():
+                                link = urljoin(base_url, a['href'])
+                                links.append((link, a.text))
+    return links
+
 
 def download_pdfs(links, directory):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    for link, _ in links:
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    }
+    for link, name in links:
         response = requests.get(link, headers=headers, stream=True)
-        filename = os.path.join(directory, link.split("/")[-1])
-        with open(filename, 'wb') as f:
+        filename = get_pdf_filename(name, link, directory)
+        with open(filename, "wb") as f:
             for chunk in response.iter_content(chunk_size=8192):
                 if chunk:  # filter out keep-alive new chunks
                     f.write(chunk)
+
+def get_year(name):
+    # Extract year from name based on year keywords
+    for year_keyword in years_keywords:
+        if year_keyword in name:
+            return year_keyword
+    return "YEAR"
+
+def get_tu_code(link):
+    # Retrieve TU code from sfcr_sites.json based on the link
+    for site in sfcr_sites:
+        if site["url_main_page"] in link:
+            tu_code = site["tu_code"]
+            return tu_code
+    return "TU_CODE"
+   
+
+def get_name(link):
+    # Retrieve name from sfcr_sites.json based on the link
+    for site in sfcr_sites:
+        if site["url_main_page"] in link:
+            name = site["name"]
+            return name
+    return "NAME"
+
+
+def get_pdf_filename(name, link, directory):
+    filename = link.split("/")[-1]
+    if any(word in name.lower() for word in opinions_keywords + opinions_keywords_uni):
+        tu_code = get_tu_code(link)
+        year = get_year(name)
+        tu_name = get_name(link)
+        filename = f"[{year}]OPINIA[{tu_code}]_[{tu_name}].pdf"
+    elif any(word in name.lower() for word in sfcr_keywords + sfcr_keywords_uni):
+        tu_code = get_tu_code(link)
+        year = get_year(name)
+        tu_name = get_name(link)
+        filename = f"[{year}]SFCR[{tu_code}]_[{tu_name}].pdf"
+    else:
+        filename = f"[{year}]UNDEFINED[{tu_code}]_[{tu_name}].pdf"
+    return os.path.join(directory, filename)
+
 
 # Make sure the directory for the PDFs exists
 os.makedirs("pdfs", exist_ok=True)
@@ -95,9 +132,7 @@ os.makedirs("pdfs", exist_ok=True)
 for url in merged_data:
     try:
         soup = get_soup(url)
-        links = get_pdf_links(soup, url)
-        write_links_to_file(links, "scrapepdfs.txt")
+        links = get_pdf_links(soup, url, searched, years_keywords)
         download_pdfs(links, "pdfs")
     except requests.exceptions.RequestException as e:
         print(f"An error occurred with URL {url}: {e}")
-
